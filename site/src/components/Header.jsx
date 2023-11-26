54ecf36e9980c11d7a7768539debaaead1b06b18
Tomáš Batelka <tomas.batelka@email.cz>
2023-11-26 15:45:37 +0100

    asfasdfs

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   Box,
   Flex,
@@ -7,26 +5,29 @@ import {
   useColorModeValue,
   Stack,
   useColorMode,
-} from '@chakra-ui/react'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import '@fontsource/silkscreen';
 
 export default function Header() {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>MDSPlayer</Box>
-
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
-            </Stack>
-          </Flex>
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <Stack direction={"row"} alignItems={"center"} height={"100%"}>
+          <Image src="pexel.svg" height="100%" p={3} />
+          <Text fontSize={30} fontFamily={"Silkscreen"}>Pexel</Text>
+        </Stack>
+        <Flex alignItems={"center"}>
+          <Stack direction={"row"} spacing={7}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+          </Stack>
         </Flex>
-      </Box>
-    </>
-  )
+      </Flex>
+    </Box>
+  );
 }
--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import App from "./App.jsx";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
