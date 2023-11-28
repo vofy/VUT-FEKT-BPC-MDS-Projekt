1e5df54c41fe3fb2cd84a591e16bbd5ef034ec8a
Tomáš Batelka <tomas.batelka@email.cz>
2023-11-28 14:35:40 +0100

    fsad

--- ffmpeg-streams.py
+++ ffmpeg-streams.py
@@ -20,26 +20,16 @@ def main():
         toml.dump(config, file)
 
     for stream in config["input"]:
-        src_scale = int(subprocess.Popen(["ffprobe", "-v", "error", 
-                                            "-select_streams",  "v:0", 
-                                            "-show_entries", "stream=height", 
-                                            "-of", "csv=s=x:p=0", str(stream["src"])], 
-                                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout.read())
+                    
+        ffstream = ffmpeg.input(stream["src"], loop=1, use_wallclock_as_timestamps=1)
             
-        ffstream_in = ffmpeg.input(stream["src"], loop=1, use_wallclock_as_timestamps=1)
-            
-        ffstreams_out = []
-
-        for scale in config["output"]["scales"]:
-            if (scale <= src_scale):
-                ffstreams_out.append(
-                    ffmpeg.output(ffstream_in, 
-                                  "{}/{}_{}.flv".format(config["output"]["dst"], stream["uuid"], scale), 
-                                  vcodec="libx264", 
-                                  preset="veryfast", 
-                                  r=10))
-            
-        ffstream = ffmpeg.merge_outputs(*ffstreams_out)
+        ffstream = ffmpeg.output(ffstream_in, 
+            "{}/{}.flv".format(config["output"]["dst"], stream["uuid"]), 
+            vcodec="libx264", 
+            preset="veryfast", 
+            r=10
+        )
+        
         # ffmpeg.run_async(ffstream, quiet=True)
         print(" ".join(ffmpeg.get_args(ffstream)))
 
